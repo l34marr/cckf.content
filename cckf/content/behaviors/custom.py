7971258cc83df7e236bdd44e28fd0f51251911a1
TsungWei Hu <marr.tw@gmail.com>
2014-06-26 13:24:21 +0800

    Add HintImage Behavior.

---
+++ cckf/content/behaviors/custom.py
@@ -0,0 +1,36 @@
+from zope.interface import alsoProvides, implements
+from zope.component import adapts
+from zope import schema
+from plone.supermodel import model
+from plone.dexterity.interfaces import IDexterityContent
+from plone.autoform.interfaces import IFormFieldProvider
+
+from plone.namedfile import field as namedfile
+
+from cckf.content import MessageFactory as _
+
+
+class IHintImage(model.Schema):
+
+    image = namedfile.NamedBlobImage(
+        title=_(u"Hint Image"),
+        description=u"",
+        required=False,
+    )
+
+    image_caption = schema.TextLine(
+        title=_(u"Hint Image Caption"),
+        description=u"",
+        required=False,
+    )
+
+alsoProvides(IHintImage, IFormFieldProvider)
+
+
+class HintImage(object):
+    implements(IHintImage)
+    adapts(IDexterityContent)
+
+    def __init__(self, context):
+        self.context = context
+
