6d1927b492571dd497f6d70a1f52dd8c89e96132
TsungWei Hu <marr.tw@gmail.com>
2014-07-14 14:12:26 +0800

    Add Grant Content Type.

--- cckf/content/content.py
+++ cckf/content/content.py
@@ -1,8 +1,12 @@
 from zope.interface import implements
 from plone.dexterity.content import Item
 
+from cckf.content.interfaces import IGrant
 from cckf.content.interfaces import ISinology
 
+
+class Grant(Item):
+    implements(IGrant)
 
 class Sinology(Item):
     implements(ISinology)
--- cckf/content/interfaces.py
+++ cckf/content/interfaces.py
@@ -1,6 +1,9 @@
 from plone.directives import form
 
 
+class IGrant(form.Schema):
+    form.model('models/grant.xml')
+
 class ISinology(form.Schema):
     """Sinology Type."""
     form.model('models/sinology.xml')
