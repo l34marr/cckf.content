0c96139609ad05388eaf93375cd9d692d9a9aab2
TsungWei Hu <marr.tw@gmail.com>
2014-12-26 10:56:35 +0800

    Add News Content Type.

--- cckf/content/content.py
+++ cckf/content/content.py
@@ -1,9 +1,13 @@
 from zope.interface import implements
 from plone.dexterity.content import Item
 
+from cckf.content.interfaces import INews
 from cckf.content.interfaces import IGrant
 from cckf.content.interfaces import ISinology
 
+
+class News(Item):
+    implements(INews)
 
 class Grant(Item):
     implements(IGrant)
--- cckf/content/interfaces.py
+++ cckf/content/interfaces.py
@@ -1,7 +1,12 @@
 from plone.directives import form
 
 
+class INews(form.Schema):
+    """News Type."""
+    form.model('models/news.xml')
+
 class IGrant(form.Schema):
+    """Grant Type."""
     form.model('models/grant.xml')
 
 class ISinology(form.Schema):
