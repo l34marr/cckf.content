bb6a77096e4a1a587e3e66306d49c982614b69a8
TsungWei Hu <marr.tw@gmail.com>
2015-03-19 07:58:23 +0000

    Add Activity Content Type.

--- cckf/content/content.py
+++ cckf/content/content.py
@@ -2,12 +2,16 @@ from zope.interface import implements
 from plone.dexterity.content import Item
 
 from cckf.content.interfaces import INews
+from cckf.content.interfaces import IActivity
 from cckf.content.interfaces import IGrant
 from cckf.content.interfaces import ISinology
 
 
 class News(Item):
     implements(INews)
+
+class Activity(Item):
+    implements(IActivity)
 
 class Grant(Item):
     implements(IGrant)
--- cckf/content/interfaces.py
+++ cckf/content/interfaces.py
@@ -2,14 +2,18 @@ from plone.directives import form
 
 
 class INews(form.Schema):
-    """News Type."""
+    """News Type"""
     form.model('models/news.xml')
 
+class IActivity(form.Schema):
+    """Activity Type"""
+    form.model('models/activity.xml')
+
 class IGrant(form.Schema):
-    """Grant Type."""
+    """Grant Type"""
     form.model('models/grant.xml')
 
 class ISinology(form.Schema):
-    """Sinology Type."""
+    """Sinology Type"""
     form.model('models/sinology.xml')
 
