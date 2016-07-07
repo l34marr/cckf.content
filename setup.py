0d0b33ada43ec33c9e6de262edf4626ed0966200
TsungWei Hu <marr.tw@gmail.com>
2016-07-07 01:29:04 +0000

    Update Metadata Profile Version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.2'
+version = '0.3'
 
 setup(name='cckf.content',
       version=version,
@@ -28,17 +28,10 @@ setup(name='cckf.content',
           'setuptools',
           'plone.app.dexterity',
           'plone.namedfile [blobs]',
-          'plone.formwidget.multifile',
       ],
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      # The next two lines may be deleted after you no longer need
-      # addcontent support from paster and before you distribute
-      # your package.
-#     setup_requires=["PasteScript"],
-#     paster_plugins = ["ZopeSkel"],
-
       )
